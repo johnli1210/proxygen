f3b077a215b5d424064c0d03a5df1525dff3ec13
johnlee1210 <johnli1210@hotmail.com>
2019-02-08 15:49:14 +0800

    compile problems

---
+++ proxygen/conf.sh
@@ -0,0 +1,1 @@
+./configure CPPFLAGS='-I/usr/local/folly/include -I/usr/local/fizz/include -I/usr/local/wangle/include' LDFLAGS='-L/usr/local/folly/lib -L/usr/local/fizz/lib -L/usr/local/wangle/lib'
